b5f9f9f2b9f569e54feae86be69000def06956e6
vivirus06 <vikhurtina06@yandex.ru>
2025-03-30 16:46:02 +0300

    done

---
+++ 1 блок 5 задание.py
@@ -0,0 +1,28 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 2 + 2
+4
+>>> 5 * 7
+35
+>>> 10 / 2
+5.0
+>>> x = 10
+>>> y = 5
+>>> x + y
+15
+>>> x * y
+50
+>>> 2 ** 10
+1024
+>>> 2 ** 500
+3273390607896141870013189696827599152216642046043064789483291368096133796404674554883270092325904157150886684127560071009217256545885393053328527589376
+>>> 1 / 0
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    1 / 0
+ZeroDivisionError: division by zero
+>>> z
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    z
+NameError: name 'z' is not defined
---
+++ 1 блок 6 задание.py
@@ -0,0 +1,6 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> L = [1, 2]
+>>> L.append(L)
+>>> L
+[1, 2, [...]]
